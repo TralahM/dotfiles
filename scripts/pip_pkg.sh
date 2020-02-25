9dbf5531beeab85bc8c3b9f33d756324ba43403a
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2020-02-26 02:21:48 +0300

    csvkit python module

--- scripts/pip_pkg.sh
+++ scripts/pip_pkg.sh
@@ -2,7 +2,7 @@
 echo "upgrading pip..."
 pip install --upgrade pip
 echo "youtube downloader"
-pip install youtube-dl --upgrade
+pip install youtube-dl csvkit --upgrade
 echo "pip fabric neovim-rpc scrapy ansible pylint autopep8..."
 pip install fabric neovim ansible scrapy pylint autopep8 --upgrade
 
