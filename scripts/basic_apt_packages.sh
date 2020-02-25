563114c45cdc47e7ea0fbd13a8bfaecf4edf867d
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2020-02-25 04:31:28 +0300

    bash shell scripts

---
+++ scripts/basic_apt_packages.sh
@@ -0,0 +1,4 @@
+#!/bin/env sh
+
+sudo apt-get install zsh clang++ tcpdump ruby perl texlive-full tmux emacs \
+neovim fftw2 ffmpeg imagemagick -y
---
+++ scripts/pip_pkg.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+pip install fabric neovim ansible scrapy pylint autopep8 --upgrade
